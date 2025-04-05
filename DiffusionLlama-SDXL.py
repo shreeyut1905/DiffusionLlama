69fce60d92591d34a98d964f8b5c107a8211771a
shreeyut1905 <shreeyut.m2005@gmail.com>
2025-04-05 11:04:24 +0000

    prompts

---
+++ DiffusionLlama-SDXL.py
@@ -0,0 +1,107 @@
+import os 
+import gradio as gr 
+import random 
+import torch 
+import cv2 
+import re
+import uuid 
+import json 
+import pickle 
+from PIL import Image,ImageDraw,ImageOps,ImageFont 
+import math 
+import numpy as np 
+import argparse 
+import inspect 
+import tempfile
+
+from diffusers import StableDiffusionPipeline, StableDiffusionInpaintPipeline, StableDiffusionInstructPix2PixPipeline
+from diffusers import EulerAncestralDiscreteScheduler, PNDMScheduler
+from diffusers import StableDiffusionControlNetPipeline, ControlNetModel, DiffusionPipeline, UniPCMultistepScheduler
+from diffusers import StableDiffusionXLPipeline, AutoPipelineForText2Image
+
+
+from langchain.agents.initialize import initialize_agent
+from langchain.agents.tools import Tool 
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain_groq import ChatGroq  
+
+from sentence_transformers import SentenceTransformer
+from compel  import Compel,ReturnedEmbeddingsType
+
+PREFIX = """DIffusionLlama is designed to be able to assist user in generating high-quality images. 
+Human may provide some text prompts to DIffusionLlama.The input prompts will be analyzed by DIffusionLlama to select most suitable generative models for generating images.
+Overall,DIffusionLlama is powerful image generation system that can assist  in procesing various forms of textual input and match them  with the most suitable  generative model to accomplish the generation task.
+TOOLS:
+-----
+DIffusionLlama has access to the following tools:"""
+FORMAT_TOOLS = """To Use a Tool please Use a following format:
+```
+Though: Do I need to use a tool?Yes
+Action: the action to take,should be one of [{tool_names}]
+Action Input : the input to the action
+Observation: the result of the action
+```
+When you have response to say to Human,or if you do not need to use a tool,you must use the format:
+```
+Thought:Do I need to use a tool?No
+{ai_prefix}:[your response here]
+```
+"""
+SUFFIX = """You are very strict to filename correctness and will never fake a file name if it does not exist.
+You will remember to provide the image the image file name loyally if it's provided in the last tool observation.
+
+Begin!
+
+Previous conversation history:
+{chat_history}
+New input:{input}
+DIffusionLlama must use tools to observe images rather than imagination.
+The thoughts and observations are only visible for DiffusionLlama,DiffusionLlama should remember to repeat important information in the final response for Human.
+Thought:Do I need to use a tool?{agent_scratchpad} Let's think step by step."""
+TOT_PROMPTS = """Identify and behave as five different experts that are appropriate to select one element from the input list that best matches the input prompt.
+All experts will write down the selection result, then share it with the group.
+You then analyze all 5 analyses and output the consensus selected element or your best guess matched element.
+The final selection output must be the same as the TEMPLATE:
+TEMPLATE:
+```
+Selected:[the selected word]
+```
+Input list:{search_list}
+
+Input prompt:{input}
+"""
+PROMPT_PARSE_PROMPTS = """Given the user input text.
+Please judge the paradigm of the input text,and then recognize the main string of text prompts according to the corresponding form.
+The output must be same as  the TEMPLATE:
+TEMPLATE:
+```
+Prompts:[the output prompts]
+```
+For instance:
+1. Input: A dog
+   Prompts: A dog 
+2. Input: generate an image of a dog
+   Prompts: an image of a dog
+3. Input: I want to see a beach
+   Prompts: a beach
+4. Input: If you give me a toy, I will laugh very happily
+   Prompts: a toy and a laugh face
+
+Input: {inputs}
+"""
+TREE_OF_MODEL_PROMPT_SUBJECT = """You are information analyst who can analyze and abstract a set of words to abstract some representation categories.
+Below is a template that can respresent the abstracted categories in Subject Dimension belonging to concrete noun:
+TEMPLATE:
+```
+Categories :
+-[Subject]
+-[Subject]
+-...
+```
+You MUST abstract the categories in a highly abstract manner only from Subject Dimension and ensure the whole number of Categories are fewer than 5.
+Then ,  You MUST remove the style-related categories.
+Please output the categories following the format of TEMPLATE.
+Input : {input}
+"""
+
+TREE_OF_MODEL_PROMPT_STYLE = """"""
